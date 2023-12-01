d87a59d593df21de61c12226437dc2d23f9e8203
Viktoria Wieser <viktoria.wieser@stud-mail.uni-wuerzburg.de>
2023-12-01 16:28:52 +0100

    tag 1 fertig :)

--- tag1/viki.py
+++ tag1/viki.py
@@ -13,10 +13,10 @@ for line in file.splitlines():
     sum += res
 print("puzzle 1 sum", sum)
 
-#part 2 (words and digits)
+# part 2 (words and digits)
 
 def word_at(line, index, word):
-   if len(word)+index >= len(line):
+   if len(word)+index > len(line):
        return False
    return word in line[index:index+len(word)]
 
@@ -25,12 +25,16 @@ for line in file.splitlines():
     first = -1
     last = -1
     for i in range (len(line)):
-        for x, word in enumerate(["zero","one","two", "three", "four", "five", "six", "seven", "eight", "nine"]):
+        for x, word in enumerate(["one","two", "three", "four", "five", "six", "seven", "eight", "nine"]):
             if word_at(line,i,word):
                 if first == -1:
-                    first = x
-                last = x
-        sum += first*10 * last
+                    first = x+1
+                last = x+1
+        if line[i].isdigit():
+            if first == -1:
+                first = int(line[i])
+            last = int(line[i])
+    sum += first*10 + last
 print("part 2 sum", sum)
 
 
