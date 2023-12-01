c33eb508b7df16a5197652a41f6eb907cf97ee01
Emi <emanuel.boehm@gmail.com>
2023-12-01 18:50:15 +0100

    part two improved

--- tag1/emi/src/main.rs
+++ tag1/emi/src/main.rs
@@ -1,5 +1,8 @@
 use std::fs;
 
+const WORDS: [&'static str; 9] = [
+    "one", "two", "three", "four", "five", "six", "seven", "eight", "nine",
+];
 fn main() {
     let content = fs::read_to_string("input_emi.txt").expect("File could not be read");
     let sum = part_one(&content);
@@ -8,15 +11,12 @@ fn main() {
     println!("part two: {sum}");
 }
 fn begins_with_number(input: &str) -> Option<u32> {
-    let words = [
-        "one", "two", "three", "four", "five", "six", "seven", "eight", "nine",
-    ];
     let first_char = input.chars().next().unwrap();
     let maybe_digit = first_char.to_digit(10);
     if maybe_digit.is_some() {
         return maybe_digit;
     }
-    for (index, word) in words.into_iter().enumerate() {
+    for (index, word) in WORDS.into_iter().enumerate() {
         if input.starts_with(word) {
             return Some(index as u32 + 1);
         }
