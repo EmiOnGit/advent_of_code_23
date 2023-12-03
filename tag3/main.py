e3883ef2a80fdf22f75521434b60824cf1bd2a82
Viktoria Wieser <viktoria.wieser@stud-mail.uni-wuerzburg.de>
2023-12-03 19:24:11 +0100

    final part 2

--- tag3/main.py
+++ tag3/main.py
@@ -1,7 +1,9 @@
-import enum
+
+from itertools import product
 
 
-file = open("input_viki.txt","r")
+path = "input_viki.txt"
+file = open(path,"r")
 input = file.read()
 # contains array with tupel (index,number) for every line
 num_arr = []
@@ -59,12 +61,23 @@ def part2():
             end = star_index + 1
 
             numbers_hit = []
+            
+            """
             for i in [max(line_number-1,0), line_number, min(line_number+1,len(num_arr)-1)]:
-                numbers_hit.extend([True for x in char_arr[i] if x in range(start,end)])
+                for num in num_arr[i]:
+                    if (num[0] < end) & (num[0]+len(str(num[1])) > start):
+                        numbers_hit.append(num[0])
+            """        
+        
+            for i in [line_number-1, line_number, line_number+1]:
+                if (i < 0) | (i > len(num_arr)): continue
+                line_hits = [n[1] for n in num_arr[i] if (n[0] < end + 1) & (n[0] + len(str(n[1])) > start )]
+                numbers_hit.extend(line_hits)
+            if len(numbers_hit) == 2:
+                ratio = numbers_hit[0]*numbers_hit[1]
+                sum += ratio
+    return sum
 
-
-
-                
 
 parse()
 print("Part 1 solution:",part1())
