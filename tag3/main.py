1b3568349f54597097c87a599f5bb8003e871db2
Viktoria Wieser <viktoria.wieser@stud-mail.uni-wuerzburg.de>
2023-12-03 18:43:23 +0100

    update part 2

--- tag3/main.py
+++ tag3/main.py
@@ -1,16 +1,19 @@
+import enum
+
+
 file = open("input_viki.txt","r")
 input = file.read()
+# contains array with tupel (index,number) for every line
+num_arr = []
+# contains array of index of chars for every line
+char_arr = []
+star_arr = []
 
-
-def part1():
-    # contains array with tupel (index,number) for every line
-    num_arr = []
-    # contains array of index of chars for every line
-    char_arr = []
-    sum = 0
+def parse():
     for line in input.splitlines():
         num_line_arr = []
         char_line_arr = []
+        star_line_arr = []
         new_number = ""
         for i,c in enumerate(line):
             if c.isnumeric():
@@ -19,9 +22,11 @@ def part1():
                 if new_number!="":
                     n = int(new_number)
                     num_line_arr.append((i-len(new_number),n))
-                    new_number = ""
+                    new_number = ""  
             else:
                 char_line_arr.append(i)
+                if c == "*":
+                    star_line_arr.append(i)
                 if new_number!="":
                     n = int(new_number)
                     num_line_arr.append((i-len(new_number),n))
@@ -31,7 +36,9 @@ def part1():
             num_line_arr.append((i-len(new_number),n))
         num_arr.append(num_line_arr)
         char_arr.append(char_line_arr)
+        star_arr.append(star_line_arr)
 
+def part1():
     sum = 0
     for line_number, tuples in enumerate(num_arr):
         for number_entry in tuples:
@@ -42,5 +49,23 @@ def part1():
                 if any([True for x in char_arr[i] if x in range(start,end)]):
                         sum += number_entry[1]
                         break
+    return sum
 
-print(sum)
+def part2():
+    sum = 0
+    for line_number, star_line in enumerate(star_arr):
+        for star_index in star_line:
+            start = max(star_index -1,0)
+            end = star_index + 1
+
+            numbers_hit = []
+            for i in [max(line_number-1,0), line_number, min(line_number+1,len(num_arr)-1)]:
+                numbers_hit.extend([True for x in char_arr[i] if x in range(start,end)])
+
+
+
+                
+
+parse()
+print("Part 1 solution:",part1())
+print("Part 2 solution:",part2())
